de0d660a05b344ba7f9529a2a2a3f70eec65573e
WhatsAppIT <it.technology@internet.ru>
2023-10-06 10:13:44 +0400

    213

--- controllers/cards.js
+++ controllers/cards.js
@@ -70,49 +70,61 @@ const deleteCardId = async (req, res) => {
     });
 }; */
 
-const deleteCardsIdLikes = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
-    { new: true }
-  )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        return res.status(404).send({
-          message: "Карточка с указанным _id не найдена.",
-        });
-      }
-      if (err.name === "ValidationError") {
-        return res.status(400).send({
-          message: "Переданы некорректные данные для снятии лайка. ",
-        });
-      }
+const deleteCardsIdLikes = async (req, res) => {
+  try {
+    const deleteLike = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } }, // убрать _id из массива
+      { new: true }
+    );
 
-      return res.status(500).send("Ошибка со стороны сервера.");
-    });
+    if (!deleteLike) {
+      throw new Error("NotFound");
+    }
+
+    return res.send(deleteLike);
+  } catch (error) {
+    if (error.message === "NotFound") {
+      return res.status(404).send({
+        message: "Карточка с указанным _id не найдена.",
+      });
+    }
+    if (err.name === "ValidationError") {
+      return res.status(400).send({
+        message: "Переданы некорректные данные для снятии лайка. ",
+      });
+    }
+
+    return res.status(500).send("Ошибка со стороны сервера.");
+  }
 };
 
-const putCardsIdLikes = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-    { new: true }
-  )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        return res.status(404).send({
-          message: "Карточка с указанным _id не найдена.",
-        });
-      }
-      if (err.name === "ValidationError") {
-        return res.status(400).send({
-          message: "Переданы некорректные данные для снятия лайка. ",
-        });
-      }
-      return res.status(500).send("Ошибка со стороны сервера.");
-    });
+const putCardsIdLikes = async (req, res) => {
+  try {
+    const putLike = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+      { new: true }
+    );
+
+    if (!putLike) {
+      throw new Error("NotFound");
+    }
+
+    return res.send(putLike);
+  } catch (error) {
+    if (err.name === "NotFound") {
+      return res.status(404).send({
+        message: "Карточка с указанным _id не найдена.",
+      });
+    }
+    if (err.name === "ValidationError") {
+      return res.status(400).send({
+        message: "Переданы некорректные данные для снятия лайка. ",
+      });
+    }
+    return res.status(500).send("Ошибка со стороны сервера.");
+  }
 };
 
 module.exports = {
