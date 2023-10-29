1a1254e1ef0b052d940ec73942a3e32ab9cfdead
WhatsAppIT <it.technology@internet.ru>
2023-10-29 17:29:58 +0400

    sswwsws

--- controllers/users.js
+++ controllers/users.js
@@ -12,7 +12,7 @@ const RepeatError = require("../errors/RepeatError");
 const postUser = (req, res, next) => {
   const { name, about, avatar, email, password } = req.body;
 
-  bcrypt.hash(req.body.password, 10).then((hash) => {
+  bcrypt.hash(password, 10).then((hash) => {
     User.create({
       name,
       about,
