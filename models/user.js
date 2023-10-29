b35fc900147c31348d3493224dfe4d8a48394373
WhatsAppIT <it.technology@internet.ru>
2023-10-29 11:37:44 +0400

    sadad

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
-//const AuthError = require("../errors/AuthError");
+const AuthError = require("../errors/AuthError");
 
 const userSchema = new mongoose.Schema(
   {
@@ -50,12 +50,12 @@ userSchema.statics.findUserByCredentials = function findUser(email, password) {
     .select("+password")
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error("Неправильная почта или пароль."));
+        throw new AuthError("Неправильная почта или пароль.");
       }
 
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          return Promise.reject(new Error("Неправильная почта или пароль."));
+          throw new AuthError("Неправильная почта или пароль.");
         }
 
         return user;
