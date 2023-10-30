8d0570d7cca8981a148f2d515f991b4113d3a8ea
WhatsAppIT <it.technology@internet.ru>
2023-10-30 16:04:48 +0400

    dsfsdfsdfsdfsd 20

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const { PORT = 3000, MONGO_URL = "mongodb://127.0.0.1:27017/mestodb" } =
   process.env;
 
 const app = express();
-//app.use(express.json());
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
