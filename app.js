f4165f5730c8c0582088714241721d05071c84cb
WhatsAppIT <it.technology@internet.ru>
2023-10-31 10:29:04 +0400

    аываываываыва 5

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
 app.use("/users", auth, routerUsers);
 app.use("/cards", auth, routerCards);
 app.use("*", auth, (req, res) => {
-  return next(new NotFoundError("Страница не найдена"));
+  return res.statusCode(404).send("Страница не найдена");
 });
 
 app.use(errors());
