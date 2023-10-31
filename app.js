b4b7e91b757618f533d35272fde29e524f888307
WhatsAppIT <it.technology@internet.ru>
2023-10-31 10:34:34 +0400

    не уходят однотипные ошибки 5

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(
 app.use("/users", auth, routerUsers);
 app.use("/cards", auth, routerCards);
 app.use("*", auth, (req, res) => {
-  return res.status(404).send("Страница не найдена");
+  return res.status(404).send({ message: err.message });
 });
 
 app.use(errors());
