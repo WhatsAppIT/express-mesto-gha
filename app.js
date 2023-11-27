cb149439bd2ad6c70913bed9d34ad71de462bb95
WhatsAppIT <it.technology@internet.ru>
2023-11-27 20:45:40 +0400

    204527112023

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const { login, postUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const errorHandler = require('./middlewares/error-handler');
-const routerUsers = require('./routes/users');
-const routerCards = require('./routes/cards');
-const NotFoundError = require('./errors/NotFoundError');
-const { linkRegex } = require('./utils/constants');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { celebrate, Joi, errors } = require("celebrate");
+const { login, postUser } = require("./controllers/users");
+const { reqLogger, errorLogger } = require("./middlewares/logger");
+const auth = require("./middlewares/auth");
+const errorHandler = require("./middlewares/error-handler");
+const routerUsers = require("./routes/users");
+const routerCards = require("./routes/cards");
+const NotFoundError = require("./errors/NotFoundError");
+const { linkRegex } = require("./utils/constants");
 
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3001, MONGO_URL = "mongodb://127.0.0.1:27017/mestodb" } =
+  process.env;
 
 const app = express();
 
@@ -20,8 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(reqLogger);
+
 app.use(
-  '/signin',
+  "/signin",
 
   celebrate({
     body: Joi.object().keys({
@@ -29,10 +33,10 @@ app.use(
       password: Joi.string().required(),
     }),
   }),
-  login,
+  login
 );
 app.use(
-  '/signup',
+  "/signup",
 
   celebrate({
     body: Joi.object().keys({
@@ -43,22 +47,27 @@ app.use(
       password: Joi.string().required(),
     }),
   }),
-  postUser,
+  postUser
 );
 app.use(auth);
-app.use('/users', routerUsers);
+app.use("/users", routerUsers);
 
-app.use('/cards', routerCards);
+app.use("/cards", routerCards);
 
-app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+app.use("*", (req, res, next) =>
+  next(new NotFoundError("Страница не найдена"))
+);
 
+app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
 
 async function init() {
   await mongoose.connect(MONGO_URL);
+  console.log(MONGO_URL);
   await app.listen(PORT);
+  console.log(PORT);
 }
 
 init();
